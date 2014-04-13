e63609504f108cb014b9a8679bb90bb554ce2b31
Artur Sarbak <artur.sarbak@enterprisesoftware.pl>
2014-04-13 13:07:56 +0200

    Commit szablonu projektu.

---
+++ grails-app/conf/AngularResources.groovy
@@ -0,0 +1,8 @@
+modules = {
+'angular' {
+    resource url:"js/lib/angular/angular.js", nominify:true
+   }
+'ui.bootstrap' {
+	resource url:"js/lib/angular-bootstrap/ui-bootstrap.js", nominify:true
+  }
+ }
