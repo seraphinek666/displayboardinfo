9b0c6942f8795313367cff5a8b33fa2ae3a0b15a
gzbbdg <lukgrz1@wp.pl>
2014-04-29 00:52:40 +0200

    wpięty ui-router + szkielety admina
    Skonfigurowany ui-router

--- web-app/js/ng-app/application.js
+++ web-app/js/ng-app/application.js
@@ -1,67 +1,80 @@
 // W TYM PLIKU KONFIGURUJEMY PROPERTIESY I INICJALIZUJEMY MODUŁ
 
-var app = angular.module('app', [ 'ngRoute', 'ui.bootstrap',
-		'pascalprecht.translate', 'dialogs', 'ngCookies' ]);
+var app = angular.module('app', [ 'ui.bootstrap', 'pascalprecht.translate', 'dialogs',
+		'ngCookies', 'ui.router' ]);
 
+// ROUTING KONTROLER -> WIDOK
 
-//ROUTING KONTROLER -> WIDOK
+app.config(function($stateProvider, $urlRouterProvider) {
+	//
+	// For any unmatched url, redirect to /state1
+	$urlRouterProvider.otherwise("");
+	//
+	// Now set up the states
+	$stateProvider.state('root', {
+		url : "",
+		templateUrl : "view/main/main.html"
+	}).state('admin', {
+		url : "/admin",
+		templateUrl : "view/admin/admin.html"
+	}).state('admin.dashboardy', {
+		url : "/dashboardy",
+		templateUrl : "view/admin/dashboardy.html"
+	})	.state('admin.uzytkownicy', {
+		url : "/uzytkownicy",
+		templateUrl : "view/admin/uzytkownicy.html"
+	}).state('admin.pacjenci', {
+		url : "/pacjenci",
+		templateUrl : "view/admin/pacjenci.html"
+	}).state('admin.lekarze', {
+		url : "/lekarze",
+		templateUrl : "view/admin/lekarze.html"
+	}).state('admin.harmonogramy', {
+		url : "/harmonogramy",
+		templateUrl : "view/admin/harmonogramy.html"
+	}).state('admin.gabinety', {
+		url : "/gabinety",
+		templateUrl : "view/admin/gabinety.html"
+	}).state('client', {
+		url : "/client",
+		templateUrl : "view/client/client.html"
 
-app.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: 'view/main/main.html',
-			controller: "MainController"
-		})
-        .when('/login', {
-            templateUrl: 'view/login/login.html',
-            controller: "LoginController"
-        })
-        .when('/admin', {
-            templateUrl: 'view/admin/admin.html',
-            controller: "AdminController"
-        })
-        .when('/client', {
-            templateUrl: 'view/client/client.html',
-            controller: "ClientController"
-        })
-		.otherwise({ redirectTo : "/"});
-}]);
+	})
+});
 
 // TŁUMACZENIA (przykładowe)
 
-app.config(function ($translateProvider) {
+app.config(function($translateProvider) {
 	$translateProvider.translations('pl', {
-		MENU: {
-            LOGO: 'DisplayBoardInfo',
-			ADMIN: 'Widok administratora'
+		MENU : {
+			LOGO : 'DisplayBoardInfo',
+			ADMIN : 'Widok administratora'
 		},
-        SUCCESS: 'Sukces',
-        FAILURE: 'BĹ‚Ä…d',
-        ACTION: 'Akcja',
-        NOT_COMPLETED: 'Nie skoĹ„czone',
-        COMPLETED: 'SkoĹ„czone',
-        SENT: 'WysĹ‚ane',
-        NOT_STARTED: 'Nie rozpoczÄ™te'
+		SUCCESS : 'Sukces',
+		FAILURE : 'BĹ‚Ä…d',
+		ACTION : 'Akcja',
+		NOT_COMPLETED : 'Nie skoĹ„czone',
+		COMPLETED : 'SkoĹ„czone',
+		SENT : 'WysĹ‚ane',
+		NOT_STARTED : 'Nie rozpoczÄ™te'
 	});
 
-
-    $translateProvider.translations('en', {
-        MENU: {
-            LOGO: 'DisplayBoardInfo',
-            ADMIN: 'Administrator View'
-        },
-        SUCCESS: 'Success',
-        FAILURE: 'Error',
-        ACTION: 'Actions',
-        NOT_COMPLETED: 'Not completed',
-        COMPLETED: 'Completed',
-        SENT: 'Sent',
-        NOT_STARTED: 'Not Started'
-    });
+	$translateProvider.translations('en', {
+		MENU : {
+			LOGO : 'DisplayBoardInfo',
+			ADMIN : 'Administrator View'
+		},
+		SUCCESS : 'Success',
+		FAILURE : 'Error',
+		ACTION : 'Actions',
+		NOT_COMPLETED : 'Not completed',
+		COMPLETED : 'Completed',
+		SENT : 'Sent',
+		NOT_STARTED : 'Not Started'
+	});
 
 	$translateProvider.preferredLanguage('en');
 });
-
 
 var DisplayBoardInfo = {};
 
@@ -89,3 +102,23 @@ DisplayBoardInfo.config = {
 
 DisplayBoardInfo.lastView = "";
 
+//app.config([ '$routeProvider', function($routeProvider) {
+//$routeProvider
+//.when('/', {
+//templateUrl: 'view/main/main.html',
+//controller: "MainController"
+//})
+//        .when('/login', {
+//            templateUrl: 'view/login/login.html',
+//            controller: "LoginController"
+//        })
+//        .when('/admin', {
+//            templateUrl: 'view/admin/admin.html',
+//            controller: "AdminController"
+//        })
+//        .when('/client', {
+//            templateUrl: 'view/client/client.html',
+//            controller: "ClientController"
+//        })
+//.otherwise({ redirectTo : "/"});
+//}]);
