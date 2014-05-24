e9f7a9e2b1a86e0143f6b933f774af3952ffd305
artursitarski <artur.sitarski@gmail.com>
2014-05-24 18:26:46 +0200

    Brakujace pliki do kalendarza

--- web-app/js/ng-app/application.js
+++ web-app/js/ng-app/application.js
@@ -1,7 +1,7 @@
 // W TYM PLIKU KONFIGURUJEMY PROPERTIESY I INICJALIZUJEMY MODUŁ
 
 var app = angular.module('app', [ 'ngRoute', 'ui.bootstrap',
-		'pascalprecht.translate', 'dialogs', 'ngCookies', 'ngTable', 'toaster']);
+		'pascalprecht.translate', 'dialogs', 'ngCookies', 'ngTable', 'toaster', 'angular-md5', 'ui.calendar']);
 
 
 //ROUTING KONTROLER -> WIDOK
