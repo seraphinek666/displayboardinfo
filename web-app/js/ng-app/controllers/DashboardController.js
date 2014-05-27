6f945322dbff457918fec4fa449352f5a7079033
gzbbdg <lukgrz1@wp.pl>
2014-05-28 00:48:21 +0200

    Postęp prac - dashboardy

--- web-app/js/ng-app/controllers/DashboardController.js
+++ web-app/js/ng-app/controllers/DashboardController.js
@@ -1,15 +1,43 @@
 app.controller('DashboardController', function($scope, $routeParams, $location,
 		$translate, BaseService, $dialogs, $cookieStore, $filter,
-		ngTableParams, $modal, toaster) {
+		ngTableParams, $modal, toaster, md5) {
 
 	// obsluga akcji
 	$scope.dashboards = [];
 	$scope.dashboardsLocal = [];
-
+	$scope.physicians = [];
+	$scope.rooms = [];
+		
+	BaseService.post(DisplayBoardInfo.config.url.physician.list).then(function(response) {
+		  $scope.physicians = response; 
+		});
+	
+	$scope.fetchPhysicians = function() {
+		BaseService.post(DisplayBoardInfo.config.url.physician.list).then(function(response) {
+			$scope.physicians = response;
+			});
+	};
+	
+	$scope.fetchRooms = function() {
+		BaseService.post(DisplayBoardInfo.config.url.room.list).then(function(response) {
+			 $scope.rooms = response;
+			});
+	};
+	
+	$scope.fetchPhysicians();
+	$scope.fetchRooms();
+	
 	$scope.launchModal = function() {
+		$scope.fetchPhysicians();
+		$scope.fetchRooms();
 		var modalInstance = $modal.open({
 			templateUrl : '/displayboardinfo/view/dashboard/newDashboardModal.html',
-			controller : ModalInstanceCtrl
+			controller : ModalInstanceCtrl,
+		      resolve: {
+		    	  physicians : function(){
+		    		  return $scope.physicians;
+		    	  }
+		      }
 		});
 
 		modalInstance.result.then(function($dashboard) {
@@ -18,12 +46,33 @@ app.controller('DashboardController', function($scope, $routeParams, $location,
 		});
 	};
 
-	var ModalInstanceCtrl = function($scope, $modalInstance) {
+	var ModalInstanceCtrl = function($scope, $modalInstance, physicians) {
 		$scope.dashboard = {
 			name : '',
-			id : ''
+			component : '',
+			physician : '',
+			template : ''
 		};
-
+		
+		$scope.west = {
+			type:'',
+			config:''
+		};
+		$scope.east = {
+			type:'',
+			config:''
+		};
+		$scope.north = {
+			type:'',
+			config:''	
+		};
+		$scope.south = {
+			type:'',
+			config:''	
+		};
+		
+		$scope.components= [{name: 'zegar'},{name: 'lekarz'},{name: 'gabinet'},{name: 'reklama'}];
+		$scope.physicians = physicians;
 		$scope.addDashboard = function() {
 			$modalInstance.close($scope.dashboard);
 //			BaseService.post(DisplayBoardInfo.config.url.patient.save, {
@@ -70,39 +119,8 @@ app.controller('DashboardController', function($scope, $routeParams, $location,
 				name : 'template8',
 				url : '/displayboardinfo/view/dashboard/dashboard_template8.html'
 			}
-		];
-		
-		$scope.template = $scope.templates[0];
-		
-		// obsluga combobox'ow
-		$scope.components = [
-			{
-				name: 'zegar'
-			},
-			{
-				name: 'lekarz'
-			},
-			{
-				name: 'gabinet'
-			},
-			{
-				name: 'reklama'
-			}
-		];
-		
-		$scope.doctors = [
-			{
-				name: 'lekarz1',
-				id: '1'
-			},
-			{
-				name: 'lekarz2',
-				id: '2'
-			}
-		];
-		
-		$scope.selectedComponents = [];
-		
+		];		
+			
 	};
 
 });
